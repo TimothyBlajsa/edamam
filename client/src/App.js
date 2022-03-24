7d71b2e1b42cf364337887f8bfc7d9e2dec5651e
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-24 16:16:12 -0400

    Add new Upload page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Recipes from "./pages/recipes";
+import Upload from "./pages/upload";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route exact path='' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/recipes' element={<Recipes />} />
+        <Route path='/upload' element={<Upload />} />
       </Routes>
     </Router>
     </>
--- client/src/pages/upload.js
+++ client/src/pages/upload.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Upload = () => {
     return (
         <>
-
+            <div className='App'>
+                <h1>Upload your own recipe</h1>
+            </div>
         </>
     )
 }
