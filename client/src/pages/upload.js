cbf30ef01f7f2e0fda40ef598bdd1b3097e6212c
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-24 17:49:47 -0400

    Remove unused files

--- client/src/pages/upload.js
+++ client/src/pages/upload.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+
 const Upload = () => {
+    
     return (
         <>
             <div className='App'>
                 <h1>Upload your own recipe</h1>
             </div>
+            <script src='../../src/Upload/uploadApp' />
         </>
     )
 }
