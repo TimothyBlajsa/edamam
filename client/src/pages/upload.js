6d7cc05288a1540613b58189aef5b17cdbb664f6
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-24 20:58:52 -0400

    Add localStorage for recipe form

--- client/src/pages/upload.js
+++ client/src/pages/upload.js
@@ -1,14 +1,27 @@
 import React from 'react';
-
+import RecipeForm from '../components/form-data';
 
 const Upload = () => {
+   const getInputValue = (event) => {
+    const userValue = event.target.value;
+    console.log(userValue);
+    return userValue;
+   }
+   
+ 
+   localStorage.setItem('message', 'this is a message');
+   console.log(localStorage.getItem('message'));
+   
     
     return (
         <>
             <div className='App'>
                 <h1>Upload your own recipe</h1>
+                <p></p>
+                <input type='text' onClick={getInputValue} className='search-bar'/>
+                <RecipeForm></RecipeForm>
             </div>
-            <script src='../../src/Upload/uploadApp' />
+            
         </>
     )
 }
