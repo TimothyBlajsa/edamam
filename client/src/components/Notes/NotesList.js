a309db9dab121d0eb01384fb48ad438b8380d193
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-25 10:19:00 -0400

    Add unique id to NotesList

--- client/src/components/Notes/NotesList.js
+++ client/src/components/Notes/NotesList.js
@@ -1,19 +1,20 @@
 import Note from './Note';
 import AddNote from './AddNote';
-
+import { v4 as uuidv4 } from 'uuid';
 const NotesList = ({
 	notes,
 	handleAddNote,
 	handleDeleteNote,
 }) => {
 	return (
-		<div className='notes-list'>
+		<div className='notes-list' key={uuidv4()}>
 			{notes.map((note) => (
 				<Note
 					id={note.id}
 					text={note.text}
 					date={note.date}
 					handleDeleteNote={handleDeleteNote}
+					key={uuidv4()}
 				/>
 			))}
 			<AddNote handleAddNote={handleAddNote} />
