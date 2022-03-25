eeefb60b255d81edcbf37f82320e59b4c82f34ce
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-24 21:23:20 -0400

    Comment out old localstorage test

--- client/src/pages/upload.js
+++ client/src/pages/upload.js
@@ -2,15 +2,15 @@ import React from 'react';
 import RecipeForm from '../components/form-data';
 
 const Upload = () => {
-   const getInputValue = (event) => {
-    const userValue = event.target.value;
-    console.log(userValue);
-    return userValue;
-   }
+//    const getInputValue = (event) => {
+//     const userValue = event.target.value;
+//     console.log(userValue);
+//     return userValue;
+//    }
    
  
-   localStorage.setItem('message', 'this is a message');
-   console.log(localStorage.getItem('message'));
+//    localStorage.setItem('message', 'this is a message');
+//    console.log(localStorage.getItem('message'));
    
     
     return (
@@ -18,7 +18,7 @@ const Upload = () => {
             <div className='App'>
                 <h1>Upload your own recipe</h1>
                 <p></p>
-                <input type='text' onClick={getInputValue} className='search-bar'/>
+                {/*<input type='text' onClick={getInputValue} className='search-bar'/>*/}
                 <RecipeForm></RecipeForm>
             </div>
             
